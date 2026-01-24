fe0aba23f0ade6d9108ea143e5bc0cdaeb1680aa
Boredvoidater <boredvoidater@proton.me>
2026-01-24 06:57:00 +0100

    fixed hash

--- modules/home/apps/cad.nix
+++ modules/home/apps/cad.nix
@@ -7,7 +7,7 @@ let
     owner = "Seeed-Studio";
     repo = "OPL_Kicad_Library";
     rev = "master"; # You can pin this to a specific commit hash if you want stability
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Placeholder
+    sha256 = "sha256-p3Ay0Q6GIIwnFWGBXV3fZvgQ5iavaj27OIaijMZN//M="; # Placeholder
   };
 in
 {
