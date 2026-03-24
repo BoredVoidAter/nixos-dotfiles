717c68f51a8b6b1fd57248731bab14bf57c74106
BoredVoidAter <boredvoideater@proton.me>
2026-03-24 21:40:16 +0100

    force wodim

--- aspire-home.nix
+++ aspire-home.nix
@@ -1,27 +1,28 @@
 { config, pkgs, lib, ... }:
 
+let
+  fake-cdrecord = pkgs.writeShellScriptBin "cdrecord" ''
+    echo "cdrecord disabled, use wodim"
+    exit 1
+  '';
+in
 {
   imports = [
-    # Desktop
     ./modules/home/desktop/theme.nix
     ./modules/home/desktop/qtile.nix
     ./modules/home/desktop/rofi.nix
     ./modules/home/desktop/polkit.nix
     ./modules/home/desktop/nvim.nix
-    
-    # Terminal
     ./modules/home/terminal/git.nix
     ./modules/home/terminal/bash.nix
     ./modules/home/terminal/fzf.nix
-    
-    # Apps (Stripped down)
     ./modules/home/apps/neovim.nix
     ./modules/home/apps/firefox.nix
     ./modules/home/apps/media.nix
   ];
 
-  # Basic lightweight apps instead of your heavy misc.nix
   home.packages = with pkgs; [
+    fake-cdrecord  # Shadows brasero's bundled cdrecord, forcing wodim
     brightnessctl
     wireplumber
     lxappearance
@@ -31,17 +32,13 @@
     pamixer
     repomix
     localsend
-    
-    # Optical Media Playback & Extraction
     vlc
     libdvdcss
     libdvdread
     libdvdnav
-    asunder      # Graphical Audio CD Ripper
-    handbrake    # Video DVD Ripper
-    vcdimager    # Special tool for extracting SVCD/VCD .mpg files
-    
-    # CD Burning & YouTube Downloading
+    asunder
+    handbrake
+    vcdimager
     yt-dlp
     ffmpeg
     brasero
